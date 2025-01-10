28e7c0375a0ea42fa9eef6b57fc478459b0ad407
Gurudevi s b <131776842+Gurudevisb@users.noreply.github.com>
2025-01-10 19:39:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/',
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects', // Make sure this file exists in the project root
-          dest: '.', // Copy it to the root of the dist folder
-        },
-      ],
-    }),
-  ],
-  server: {
-    host: true,
-    port: 5173,
-  },
   build: {
-    outDir: 'dist', // Build output directory
+    outDir: 'dist',
   },
 });
